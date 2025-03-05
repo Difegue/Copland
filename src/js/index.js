4b82244a19c9dda74eb71541fea693a7bca19c58
Difegue <sugoi@cock.li>
2025-03-05 01:15:38 +0100

    Add reset defaults button to clay config

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,27 @@ var Clay = require('pebble-clay');
 // Load our Clay configuration file
 var clayConfig = require('./config.json');
 // Initialize Clay
-var clay = new Clay(clayConfig);
+var clay = new Clay(clayConfig, function(minified) {
+    var clayConfig = this;
+    var _ = minified._;
+    var $ = minified.$;
+    var HTML = minified.HTML;
+  
+    function resetSettings() {
+        clayConfig.getItemByMessageKey('showDate').set(true);
+        clayConfig.getItemByMessageKey('showSeconds').set(true);
+      
+        clayConfig.getItemByMessageKey('bgColor').set('FFFFFF');
+        clayConfig.getItemByMessageKey('hoursColor').set('00AA55');
+        clayConfig.getItemByMessageKey('minutesColor').set('FF5555');
+        clayConfig.getItemByMessageKey('secondsColor').set('FFAA00');
+        clayConfig.getItemByMessageKey('dateColor').set('0055FF');
+    }
+  
+    clayConfig.on(clayConfig.EVENTS.AFTER_BUILD, function() {
+        var resetSettingsBtn = clayConfig.getItemById('resetDefaults');
+        resetSettingsBtn.on('click', resetSettings);
+    });
+    
+  });
 
